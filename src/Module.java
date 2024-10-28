540f674a28d192b77caac26129b06c8415a68009
NeGeterohromat <140048641+NeGeterohromat@users.noreply.github.com>
2024-10-28 18:21:41 +0500

    task1

--- src/Module.java
+++ src/Module.java
@@ -3,17 +3,17 @@ import java.util.HashMap;
 public class Module {
     private final String name;
 
-    private HashMap<Student,ModuleScores> studentsScores;
+    private HashMap<Student, ModuleScores> studentsScores;
 
-    public Module(String name){
+    public Module(String name) {
         this.name = name;
     }
 
-    public void addStudent(Student student, ModuleScores scores){
-        studentsScores.put(student,scores);
+    public void addStudent(Student student, ModuleScores scores) {
+        studentsScores.put(student, scores);
     }
 
-    public ModuleScores getScores(Student student){
+    public ModuleScores getScores(Student student) {
         return studentsScores.getOrDefault(student, null);
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,14 +1,14 @@
 public class Student {
     private final int ulearnID;
 
-    public Student(String ulearnID){
+    public Student(String ulearnID) {
         this.ulearnID = hashCode(ulearnID);
     }
 
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof Student) {
-            return ((Student) obj).ulearnID==ulearnID;
+            return ((Student) obj).ulearnID == ulearnID;
         }
         return false;
     }
@@ -18,7 +18,7 @@ public class Student {
         return ulearnID;
     }
 
-    private int hashCode(String s){
+    private int hashCode(String s) {
         return s.hashCode();
     }
 }
