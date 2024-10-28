9b340b5dd07aa8c6048b03f702092b27112aa4df
NeGeterohromat <140048641+NeGeterohromat@users.noreply.github.com>
2024-10-28 18:14:52 +0500

    task1code

---
+++ src/Module.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class Module {
+    private final String name;
+
+    private HashMap<Student,ModuleScores> studentsScores;
+
+    public Module(String name){
+        this.name = name;
+    }
+
+    public void addStudent(Student student, ModuleScores scores){
+        studentsScores.put(student,scores);
+    }
+
+    public ModuleScores getScores(Student student){
+        return studentsScores.getOrDefault(student, null);
+    }
+}
---
+++ src/ModuleScores.java
@@ -0,0 +1,23 @@
+public class ModuleScores {
+    private final int questionsScore;
+    private final int exercisesScore;
+    private final int practiceScore;
+
+    public ModuleScores(int questionsScore, int exercisesScore, int practiceScore) {
+        this.questionsScore = questionsScore;
+        this.exercisesScore = exercisesScore;
+        this.practiceScore = practiceScore;
+    }
+
+    public int getQuestionsScore() {
+        return questionsScore;
+    }
+
+    public int getExercisesScore() {
+        return exercisesScore;
+    }
+
+    public int getPracticeScore() {
+        return practiceScore;
+    }
+}
